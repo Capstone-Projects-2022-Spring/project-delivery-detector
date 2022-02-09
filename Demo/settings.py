fe0ed41ebc8e8de1b2e0c2d040c9b674eee5af5a
johng <johnglatts1@hotmail.com>
2022-02-08 23:11:28 -0500

    update settings.py to allow hosting public LAN

--- Demo/settings.py
+++ Demo/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '4@)50l3)8z#mu3vrda*l7-xwp(clt_9dfzuc&7j3x&=n%7n_t@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.1.156"]
 
 # Application definition
 
