582880c504f288bb1ba945796770d5df05942979
johng <johnglatts1@hotmail.com>
2022-03-04 20:34:04 -0500

    Create the databases and back-end for the server

---
+++ DeliveryDetectorServer/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Database table for user accounts    
+class UserAccount(models.Model):
+    user_name = models.CharField(max_length=200)
+    user_pw = models.CharField(max_length=200)
+    user_email = models.EmailField(max_length=300)
+    user_phone = models.BigIntegerField()
+    box_number = models.ForeignKey('BoxInfo', on_delete=models.CASCADE)
+
+# Database table for boxes
+class BoxInfo(models.Model):
+    box_number = models.IntegerField()
+    date = models.DateField()
+    multi_user = models.BooleanField()
+    # need a one to many field or similar, to keep track of all users using this box 
---
+++ DeliveryDetectorServer/views.py
@@ -0,0 +1,50 @@
+# refer back to the design doc. and add the fields mentioned there 
+import json
+from django.http import JsonResponse 
+from django.http import HttpResponse
+from django.forms.models import model_to_dict
+from django.shortcuts import render
+from .forms import *
+from .models import *
+
+# Home page view
+def index(request):
+    return render(request, 'DeliveryDetectorServer/index.html')
+
+# Sign-up view
+# Will provide a form for the user to fill out
+# If the data is valid it will be saved to the production database 
+def sign_up(request):
+    form = UserAccountForm()
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['user_name']
+            pw = form.cleaned_data['user_pw']
+            email = form.cleaned_data['user_email']
+            phone = form.cleaned_data['user_phone']
+            box = BoxInfo.objects.get(pk=form.cleaned_data['box_number'])
+            new_record = UserAccount(user_name=name, user_pw=pw, user_email=email, user_phone=phone, box_number=box)
+            new_record.save()
+            return HttpResponse("User has been added to the database!")
+    return render(request, 'DeliveryDetectorServer/sign_up.html', {'form': form, 'title': 'Sign Up'})
+
+# Log-in view
+def log_in(request):
+    form = LogInForm()
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['user_name']
+            pw = form.cleaned_data['user_pw']
+            user = UserAccount.objects.get(user_name=name, user_pw=pw)
+            form = UserAccountForm()
+            form.user_name = name
+            form.user_pw = pw
+            return render(request, 'DeliveryDetectorServer/sign_up.html', {'form': form, 'title': 'Change Settings'})
+    return render(request, 'DeliveryDetectorServer/log_in.html', {'form': form})
+
+def get_user(request, name):
+     user = UserAccount.objects.get(user_name=name)
+     user_dict = model_to_dict(user) 
+     return JsonResponse(json.loads(json.dumps(user_dict)))
