5df3d500da5444cba31eb14f0f8ccb8a85d79414
johng <johnglatts1@hotmail.com>
2022-03-06 11:48:09 -0500

    add more api-call functionality

--- api_call.py
+++ api_call.py
@@ -10,15 +10,24 @@ class DeliveryDetectorBox():
 
     # Check if this box is registered to a specific user 
     def check_user_box(self, name):
+        # call the API 
         response = requests.get("http://detector-env.eba-epj2ey8y.us-east-2.elasticbeanstalk.com/get_user/" + name + "/")
         res_obj = response.json()  
+
+        # iterate through the JSON keys (database fields)
+        for key in res_obj:
+            print(str(key) + ': ' + str(res_obj[key]))
+
+        # check if this is the correct box
         if res_obj['box_number'] == self.box_number:
+            #unlock_box()
             print("UNLOCK THE BOX FOR USER: " + res_obj['user_name'])
         else:
+            #lock_box()
             print('LOCK THE BOX')
 
-
-
+ 
 if __name__ == '__main__':
+    # Create a new box DeliveryDetectorBox instance with box number 1
     box = DeliveryDetectorBox(1)
-    box.check_user_box('test')
+    box.check_user_box('test')  # the name will be found from the QR code reader
