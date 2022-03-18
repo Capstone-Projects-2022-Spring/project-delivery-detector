947910dee4b6638e3a9d90c2445e1fea6aae8982
johng <johnglatts1@hotmail.com>
2022-03-18 14:56:11 -0400

    api fix

--- DeliveryDetectorServer/views.py
+++ DeliveryDetectorServer/views.py
@@ -1,12 +1,14 @@
 # refer back to the design doc. and add the fields mentioned there 
+import os
 import json
+import urllib
 import pyqrcode
-from pyqrcode import QRCode
-from django.http import JsonResponse 
-from django.http import HttpResponse
+from django.core.files import File  
+from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 from django.core.mail import EmailMessage
 from django.shortcuts import render
+from pyqrcode import QRCode
 from .forms import *
 from .models import *
 
@@ -26,9 +28,14 @@ def sign_up(request):
             pw = form.cleaned_data['user_pw']
             email = form.cleaned_data['user_email']
             phone = form.cleaned_data['user_phone']
+            create_qr_code(name)
+            qr = 'qr_' + name + '.png'
             box = BoxInfo.objects.get(pk=form.cleaned_data['box_number'])
-            new_record = UserAccount(user_name=name, user_pw=pw, user_email=email, user_phone=phone, box_number=box)
+            new_record = UserAccount(user_name=name, user_pw=pw, 
+                                     user_email=email, user_phone=phone, 
+                                     qr_code=qr, box_number=box)
             new_record.save()
+            os.remove(qr)
             return HttpResponse("User has been added to the database!")
     return render(request, 'DeliveryDetectorServer/sign_up.html', {'form': form, 'title': 'Sign Up'})
 
@@ -50,8 +57,16 @@ def log_in(request):
 # Return a UserAccount record in JSON format 
 def get_user(request, name):
     user = UserAccount.objects.get(user_name=name)
-    user_dict = model_to_dict(user) 
-    return JsonResponse(json.loads(json.dumps(user_dict)))
+    dict = {'user_name': user.user_name, 'user_pw': user.user_pw,
+            'user_email': user.user_email, 'user_phone': user.user_phone,
+            'box_number': user.box_number.pk}
+    return JsonResponse(json.loads(json.dumps(dict)))
+
+# Generate QR code with the user name
+def create_qr_code(name):
+    qr_name = pyqrcode.create(name)
+    qr_file_name = 'qr_' + name + '.png'
+    qr_name.png(qr_file_name, scale=10)
 
 # Send a delivery alert to the user 
 def send_alert(request, name):
@@ -59,11 +74,7 @@ def send_alert(request, name):
     user = UserAccount.objects.get(user_name=name)
     phone = user.user_phone
     email = user.user_email
-    # Generate QR code with the user name
-    url = pyqrcode.create(name)
-    # Create and save the png file naming "myqr.png"
-    qr_name = 'qr_' + name + '.png'
-    url.png(qr_name, scale = 6)
+
     # Send an Email
     send_email = EmailMessage(
         subject='Delivery Detector Alert',
