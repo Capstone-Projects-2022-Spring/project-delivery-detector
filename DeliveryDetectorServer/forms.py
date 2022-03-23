de53502924c12476b760601bc2b8154acfaf5e21
tuj52977 <tuj52977@temple.edu>
2022-03-23 18:54:15 -0400

    Added seller portal where sellers can get QR codes for their products, text works need to test email still with proper authetication

--- DeliveryDetectorServer/forms.py
+++ DeliveryDetectorServer/forms.py
@@ -12,8 +12,15 @@ class UserAccountForm(forms.Form):
 class LogInForm(forms.Form):
     user_name = forms.CharField(label='User Account Name', max_length=200)
     user_pw = forms.CharField(label='Password', max_length=200)
+
 class wifi_QR_form(forms.Form):
     network_name = forms.CharField(label='Network Name', max_length=200)
     network_password = forms.CharField(label='Password', max_length=200)
     user_phone = forms.IntegerField()
+
+class seller_QR_form(forms.Form):
+    user_name = forms.CharField(label='user', max_length=200)
+    user_email = forms.EmailField(max_length=300)
+    user_phone = forms.IntegerField()
+
 
