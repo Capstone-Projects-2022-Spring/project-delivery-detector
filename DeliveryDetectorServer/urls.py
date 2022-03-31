1caa6d1ac0fcfaa17b45f64889c2b5eaf423ce40
johng <johnglatts1@hotmail.com>
2022-03-31 19:51:02 -0400

    add url for new API endpoint

--- DeliveryDetectorServer/urls.py
+++ DeliveryDetectorServer/urls.py
@@ -9,9 +9,6 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
