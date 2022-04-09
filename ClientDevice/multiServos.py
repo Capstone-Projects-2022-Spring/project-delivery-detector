df51dcc7cc935a16c05aee6fe16b12759017c2dc
tuj52977 <89864304+tuj52977@users.noreply.github.com>
2022-04-09 17:05:10 -0400

    Added servo utility file
    Within file are functions and build instructions for use of servoboard

---
+++ ClientDevice/multiServos.py
@@ -0,0 +1,42 @@
+# Build instructions
+# sudo pip3 install adafruit-circuitpython-pca9685
+# sudo pip3 install adafruit-circuitpython-motor
+# Enable I2C in preferences -> Raspberry Configurations -> Interfaces
+# or through command line $sudo raspi-config -> Interface Options 
+#
+# Servo board must be connected to GPIO 2 (SDA), GPIO 3(SCL) , 3v3 Power (pin 1 or 17) and ground
+
+
+import time
+from board import SCL, SDA
+import busio
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+def lock(servo):
+    servo.fraction = .55
+    time.sleep(.03)
+def unlock(servo):
+    servo.fraction = 0
+    time.sleep(.03)
+
+i2c = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c)
+pca.frequency = 50
+
+servo0 = servo.Servo(pca.channels[0])
+servo1 = servo.Servo(pca.channels[1])
+servo2 = servo.Servo(pca.channels[2])
+
+if __name__ == "__main__":
+    unlock(servo0)
+    unlock(servo1)
+    unlock(servo2)
+    time.sleep(2)
+    print("unlocked")
+    lock(servo0)
+    lock(servo1)
+    lock(servo2)
+    print("Locked")
+
+    pca.deinit()
